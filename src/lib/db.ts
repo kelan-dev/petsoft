f0c86d41697b45fc3695c870c2aeeb7b1b40f5d8
kelan-dev <174945686+kelan-dev@users.noreply.github.com>
2024-09-20 21:25:03 -0400

    Migrate from SQLite to Postgres

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,19 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+// Combines db.ts recommendations from both of these pages:
+// https://www.prisma.io/docs/orm/overview/databases/neon
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
 
-declare global {
-  var prisma: PrismaClient | undefined;
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
 }
 
-const prisma =
-  global.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  });
+const prismaClientSingleton = () => {
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaNeon(pool);
+  return new PrismaClient({ adapter });
+};
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+declare global {
+  var prisma: ReturnType<typeof prismaClientSingleton> | undefined;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
